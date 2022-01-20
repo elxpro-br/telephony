a1b36b7d90ad88bb953daf2633ddd50f79c9becf
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-01-20 19:18:35 -0300

    config pipeline

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Telephony.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
