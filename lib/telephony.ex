b8d2ce80b16144714d28682d9f7954e11e78bb55
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-01-20 19:35:53 -0300

    primeira aula sobre modulos e funcoes

--- lib/telephony.ex
+++ lib/telephony.ex
@@ -1,18 +1,2 @@
 defmodule Telephony do
-  @moduledoc """
-  Documentation for `Telephony`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Telephony.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
